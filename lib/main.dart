a342c53f4e53a132eb106e284db7c454b5339595
Kauan <cvitorcordeiro@gmail.com>
2024-07-15 23:27:35 -0300

    future builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import "dart:async";
 import "package:flutter/material.dart";
 import 'package:currency_conversor/Config.dart';
@@ -8,15 +7,12 @@ import "dart:convert";
 const request = "https://api.hgbrasil.com/finance?key=${Config.apiKey}";
 
 Future<void> main() async {
-
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
-
-
-Future<Map> getData() async{
+Future<Map> getData() async {
   Uri uri = Uri.parse(request);
   http.Response response = await http.get(uri);
   return json.decode(response.body);
@@ -35,14 +31,41 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        elevation:10,
+        elevation: 10,
         shadowColor: Colors.black,
         title: Text("\$ Conversor"),
         backgroundColor: Colors.amber,
         centerTitle: true,
         foregroundColor: Colors.white,
       ),
-
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao Carregar dados :(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              }else{
+                return Container(color: Colors.green,);
+              }
+          }
+        },
+      ),
     );
   }
 }
